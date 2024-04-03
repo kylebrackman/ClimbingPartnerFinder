05bf75f566418e398e2312492cec5e9860eca723
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-04-02 21:46:17 -0600

    Add button and touchable opacity for home screen test.

--- CpfFrontEnd/App.js
+++ CpfFrontEnd/App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView, TextInput, Button, TouchableOpacity } from 'react-native';
 import { nw } from 'nativewind';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [text, setText] = useState("")
+
   return (
     <SafeAreaView>
       <View >
         <Text>Hello World!</Text>
         <Text>Platform: {Platform.OS == 'ios' ? 'ios' : 'android'} </Text>
         <ScrollView>
-          <View style={{ backgroundColor: 'black', height: 1600 }} >
-            <Text style={{ color: 'white', }} > Hello World</Text>
+          <View style={{ backgroundColor: 'black', height: 1600, padding: 20 }} >
+            <TextInput
+              defaultValue={text}
+              onChangeText={txt =>setText(txt)}
+              style={{borderWidth: 1, color:'white', padding: 10, borderColor: 'white'}}
+            ></TextInput>
+            <Button title='Press Me' onPress={()=> console.log("Hello World")}></Button>
+            <TouchableOpacity onPress={()=> console.log("Hello World")}>
+              <Text style={{borderWidth: 1, color:'white', padding: 10, borderColor: 'white'}}>Press Me</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
         <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+      </View >
+    </SafeAreaView >
 
   );
 }
